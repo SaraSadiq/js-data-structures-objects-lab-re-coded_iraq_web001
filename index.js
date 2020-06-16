eeedc95f1dd1d632f956e4f5ebf20f22b83560d4
Sara Nooraldin <sarasaadiq98@gmail.com>
2020-06-16 22:10:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution in this file!
+function updateDriverWithKeyAndValue(){
+  let driver{
+    foo:"bar"
+  }
+
+
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(){
+
+}
+
+function deleteFromDriverByKey(){
+
+}
+function destructivelyDeleteFromDriverByKey(){
+
+}
